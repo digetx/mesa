3ba58ed546b1876b343664119aa9cabf421e2d73
Thierry Reding <thierry.reding@avionic-design.de>
2013-03-26 14:14:49 +0100

    tegra: Gallium driver for NVIDIA Tegra
    Add a stub Gallium driver for NVIDIA Tegra SoCs to serve as a basis for
    future development. The driver contains enough stubs to allow Wayland's
    reference compositor Weston to run, though without any actual output.
    
    Signed-off-by: Thierry Reding <thierry.reding@avionic-design.de>

---
+++ src/gallium/drivers/tegra/tegra_resource.c
@@ -0,0 +1,94 @@
+#include <stdio.h>
+
+#include "util/u_inlines.h"
+
+#include "tegra_resource.h"
+
+static boolean
+tegra_screen_can_create_resource(struct pipe_screen *pscreen,
+				 const struct pipe_resource *template)
+{
+	bool ret = TRUE;
+	fprintf(stdout, "> %s(pscreen=%p, template=%p)\n", __func__, pscreen,
+		template);
+	fprintf(stdout, "< %s() = %d\n", __func__, ret);
+	return ret;
+}
+
+static struct pipe_resource *
+tegra_screen_resource_create(struct pipe_screen *pscreen,
+			     const struct pipe_resource *template)
+{
+	struct tegra_resource *resource;
+
+	fprintf(stdout, "> %s(pscreen=%p, template=%p)\n", __func__, pscreen,
+		template);
+
+	resource = calloc(1, sizeof(*resource));
+	if (!resource) {
+		fprintf(stdout, "< %s() = NULL\n", __func__);
+		return NULL;
+	}
+
+	resource->base = *template;
+
+	pipe_reference_init(&resource->base.reference, 1);
+	resource->base.screen = pscreen;
+
+	fprintf(stdout, "< %s() = %p\n", __func__, &resource->base);
+	return &resource->base;
+}
+
+static struct pipe_resource *
+tegra_screen_resource_from_handle(struct pipe_screen *pscreen,
+				  const struct pipe_resource *template,
+				  struct winsys_handle *handle)
+{
+	struct tegra_resource *resource;
+
+	fprintf(stdout, "> %s(pscreen=%p, template=%p, handle=%p)\n",
+		__func__, pscreen, template, handle);
+
+	resource = calloc(1, sizeof(*resource));
+	if (!resource) {
+		fprintf(stdout, "< %s() = NULL\n", __func__);
+		return NULL;
+	}
+
+	resource->base = *template;
+
+	pipe_reference_init(&resource->base.reference, 1);
+	resource->base.screen = pscreen;
+
+	fprintf(stdout, "< %s() = %p\n", __func__, &resource->base);
+	return &resource->base;
+}
+
+static boolean
+tegra_screen_resource_get_handle(struct pipe_screen *pscreen,
+				 struct pipe_resource *resource,
+				 struct winsys_handle *handle)
+{
+	boolean ret = TRUE;
+	fprintf(stdout, "> %s(pscreen=%p, resource=%p, handle=%p)\n",
+		__func__, pscreen, resource, handle);
+	fprintf(stdout, "< %s() = %d\n", __func__, ret);
+	return ret;
+}
+
+static void tegra_screen_resource_destroy(struct pipe_screen *pscreen,
+					  struct pipe_resource *resource)
+{
+	fprintf(stdout, "> %s(pscreen=%p, resource=%p)\n", __func__, pscreen,
+		resource);
+	fprintf(stdout, "< %s()\n", __func__);
+}
+
+void tegra_screen_resource_init(struct pipe_screen *pscreen)
+{
+	pscreen->can_create_resource = tegra_screen_can_create_resource;
+	pscreen->resource_create = tegra_screen_resource_create;
+	pscreen->resource_from_handle = tegra_screen_resource_from_handle;
+	pscreen->resource_get_handle = tegra_screen_resource_get_handle;
+	pscreen->resource_destroy = tegra_screen_resource_destroy;
+}
