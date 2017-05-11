UIKit0/mesa-2

tegra: perform transfers with blits

src/gallium/drivers/tegra/tegra_context.h
@@ -1,6 +1,8 @@
 #ifndef TEGRA_CONTEXT_H
 #define TEGRA_CONTEXT_H
 
+#include "util/u_slab.h"
+
 #include "pipe/p_context.h"
 #include "pipe/p_state.h"
 
@@ -25,6 +27,8 @@ struct tegra_context {
 	struct tegra_channel *gr3d;
 
 	struct tegra_framebuffer_state framebuffer;
+
+	struct util_slab_mempool transfer_pool;
 
 	struct tegra_vertex_state *vs;
 	struct tegra_vertexbuf_state vbs;

src/gallium/drivers/tegra/tegra_resource.c
@@ -86,11 +86,41 @@ static void *tegra_resource_transfer_map(struct pipe_context *pcontext,
 					 const struct pipe_box *box,
 					 struct pipe_transfer **transfer)
 {
+	struct tegra_context *context = tegra_context(pcontext);
+	struct tegra_resource *resource = tegra_resource(presource);
 	void *ret = NULL;
+	struct pipe_transfer *ptrans;
+
 	fprintf(stdout, "> %s(pcontext=%p, presource=%p, level=%u, usage=%u, box=%p, transfer=%p)\n",
 		__func__, pcontext, presource, level, usage, box, transfer);
+
+	if (usage & PIPE_TRANSFER_MAP_DIRECTLY)
+		goto out;
+
+	if (drm_tegra_bo_map(resource->bo, &ret))
+		goto err;
+
+	ptrans = util_slab_alloc(&context->transfer_pool);
+	if (!ptrans)
+		goto err;
+
+	ptrans->resource = presource;
+	ptrans->level = level;
+	ptrans->usage = usage;
+	ptrans->box = *box;
+	ptrans->stride = resource->pitch;
+	ptrans->layer_stride = ptrans->stride;
+	*transfer = ptrans;
+
+out:
 	fprintf(stdout, "< %s() = %p\n", __func__, ret);
 	return ret;
+
+err:
+	if (ret)
+		drm_tegra_bo_unmap(resource->bo);
+	fprintf(stdout, "< %s() = NULL\n", __func__);
+	return NULL;
 }
 
 static void
@@ -108,6 +138,7 @@ static void tegra_resource_transfer_unmap(struct pipe_context *pcontext,
 {
 	fprintf(stdout, "> %s(pcontext=%p, transfer=%p)\n", __func__, pcontext,
 		transfer);
+	drm_tegra_bo_unmap(tegra_resource(transfer->resource)->bo);
 	fprintf(stdout, "< %s()\n", __func__);
 }
 

digetx/mesa

src/gallium/drivers/tegra/tegra_context.h
@@ -1,6 +1,8 @@
 #ifndef TEGRA_CONTEXT_H
 #define TEGRA_CONTEXT_H
 
+#include "util/slab.h"
+
 #include "pipe/p_context.h"
 #include "pipe/p_state.h"
 
@@ -24,6 +26,8 @@ struct tegra_context {
 	struct tegra_channel *gr3d;
 
 	struct tegra_framebuffer_state framebuffer;
+
+	struct slab_child_pool transfer_pool;
 
 	struct tegra_vertex_state *vs;
 	struct tegra_vertexbuf_state vbs;

src/gallium/drivers/tegra/tegra_resource.c
@@ -89,11 +89,41 @@ static void *tegra_resource_transfer_map(struct pipe_context *pcontext,
 					 const struct pipe_box *box,
 					 struct pipe_transfer **transfer)
 {
+	struct tegra_context *context = tegra_context(pcontext);
+	struct tegra_resource *resource = tegra_resource(presource);
 	void *ret = NULL;
+	struct pipe_transfer *ptrans;
+
 	fprintf(stdout, "> %s(pcontext=%p, presource=%p, level=%u, usage=%u, box=%p, transfer=%p)\n",
 		__func__, pcontext, presource, level, usage, box, transfer);
+
+	if (usage & PIPE_TRANSFER_MAP_DIRECTLY)
+		goto out;
+
+	if (drm_tegra_bo_map(resource->bo, &ret))
+		goto err;
+
+	ptrans = slab_alloc(&context->transfer_pool);
+	if (!ptrans)
+		goto err;
+
+	ptrans->resource = presource;
+	ptrans->level = level;
+	ptrans->usage = usage;
+	ptrans->box = *box;
+	ptrans->stride = resource->pitch;
+	ptrans->layer_stride = ptrans->stride;
+	*transfer = ptrans;
+
+out:
 	fprintf(stdout, "< %s() = %p\n", __func__, ret);
 	return ret;
+
+err:
+	if (ret)
+		drm_tegra_bo_unmap(resource->bo);
+	fprintf(stdout, "< %s() = NULL\n", __func__);
+	return NULL;
 }
 
 static void
@@ -109,8 +139,12 @@ tegra_resource_transfer_flush_region(struct pipe_context *pcontext,
 static void tegra_resource_transfer_unmap(struct pipe_context *pcontext,
 					  struct pipe_transfer *transfer)
 {
+	struct tegra_context *context = tegra_context(pcontext);
 	fprintf(stdout, "> %s(pcontext=%p, transfer=%p)\n", __func__, pcontext,
 		transfer);
+	drm_tegra_bo_unmap(tegra_resource(transfer->resource)->bo);
+	pipe_resource_reference(&transfer->resource, NULL);
+	slab_free(&context->transfer_pool, transfer);
 	fprintf(stdout, "< %s()\n", __func__);
 }
 